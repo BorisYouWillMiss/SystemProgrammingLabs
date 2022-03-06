66bdd9796005c8e1e2d77edde941bde2abe53d5b
Sugoma <61140354+BorisYouWillMiss@users.noreply.github.com>
2022-03-07 02:37:58 +0700

    2nd lab 2nd task done
    Selection sort implemented using assembler

--- AssemblerLab1/Assembler_Sort_Lab2/Assembler_Sort_Lab2.cpp
+++ AssemblerLab1/Assembler_Sort_Lab2/Assembler_Sort_Lab2.cpp
@@ -1,10 +1,10 @@
 ﻿#include <iostream>
 #include <time.h>
 
-void sortBubble(int *arr,int n) {
+void sortBubble(int *a,int n) {
     std::cout << "A size: " << n << "\n";
     std::cout << "A: ";
-    for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
+    for (int i = 0; i < n; i++) std::cout << a[i] << " ";
     std::cout << "\n";
 
     int c = n-1;
@@ -14,7 +14,7 @@ void sortBubble(int *arr,int n) {
 
     _asm {
         mov cl, t
-        mov esi, arr
+        mov esi, a
         mov edx, 0
         mov ch, 0
         mov eax, 0
@@ -56,11 +56,88 @@ void sortBubble(int *arr,int n) {
     }
 }
 
-void sortSelection(int* arr, int n) {
+void sortSelection(int* a, int n) {
     std::cout << "B size: " << n << "\n";
     std::cout << "B: ";
-    for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
+    for (int i = 0; i < n; i++) std::cout << a[i] << " ";
     std::cout << "\n";
+
+    int c = n - 1;
+    int ffs = 0;
+
+    //int max = 0;
+    //int maxi = 0;
+
+    _asm {
+        mov esi, a
+
+    FOROUTER:
+        mov ecx, ffs
+        cmp ecx, c
+        je END;
+
+        mov ebx, [esi]
+        mov ecx, 0
+        mov edx, 0
+
+    FORINNER:
+            mov eax, ffs
+            sub c, eax
+            cmp edx, c
+            jae NEXT
+            jge NEXT
+
+            add c, eax
+
+            IF:
+                cmp ebx, [esi + 4 * edx + 4]
+                jae IDLE
+                jge IDLE
+
+            SAVE:
+                mov ebx, [esi + 4 * edx + 4]
+                inc edx
+                mov ecx, edx
+
+                jmp FORINNER
+
+            IDLE:
+
+                inc edx
+
+                jmp FORINNER
+
+    NEXT:
+
+            push eax
+                mov eax, c
+            mov edx, [esi + 4 * eax]
+
+            mov [esi + 4 * ecx], edx
+            mov [esi + 4 * eax], ebx
+            pop eax
+            add c, eax
+
+            inc ffs
+
+            jmp FOROUTER
+    END:    
+
+    }
+    /* Выдуманный мною неоптимизированный код на сиплюсах, использованный в качестве референса
+    for (int i = 0; i < n-1; i++) { // ffs = i
+        max = a[0]; // ebx = max
+        maxi = 0;   // ecx = maxi
+        for (int j = 0; j < n - i - 1; j++) { //  i = eax  j = edx
+            if (max < a[j+1]) {
+                max = a[j+1];
+                maxi = j+1;
+            }
+        }
+        a[maxi] = a[n - i - 1];
+        a[n - i - 1] = max;
+    }
+    */
 }
 
 int main()
@@ -68,6 +145,7 @@ int main()
     int n;
     std::cout << "Enter number of elements\n";
     std::cin >> n;
+    std::cout << "\n";
 
     srand(time(NULL));
 
@@ -78,15 +156,11 @@ int main()
         B[i] = rand() % 10;
     }
 
-    int c = 0;
-
     _asm {
         push n
         push A
 		call sortBubble
 
-        mov c, eax
-        
         add esp, 8
 
     }
@@ -94,4 +168,18 @@ int main()
     std::cout << "\n";
     for (int i = 0; i < n; i++)
         std::cout << A[i] << " ";
+    std::cout << "\n\n";
+
+    _asm {
+        push n
+        push B
+        call sortSelection
+
+        add esp, 8
+    }
+    std::cout << "\n";
+    for (int i = 0; i < n; i++)
+        std::cout << B[i] << " ";
+    
+
 }
